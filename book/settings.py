66c9bcbda13c349d208c448faae1f69d75687fe9
Boomdas <90346360+Boomdas@users.noreply.github.com>
2022-03-07 20:28:56 +0530

    restapi

--- book/settings.py
+++ book/settings.py
@@ -37,6 +37,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    
+    'rest_framework',
 ]
 
 MIDDLEWARE = [
@@ -75,8 +77,11 @@ WSGI_APPLICATION = 'book.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'book',
+        'USER': 'postgres',
+        'PASSWORD': '123',
+        'HOST': 'localhost'
     }
 }
 
